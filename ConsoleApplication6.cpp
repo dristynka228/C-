0e5bebfa6629858f765e8136d7cd43072eac25fe
dristynka228 <144123819+dristynka228@users.noreply.github.com>
2024-02-13 10:44:32 +0300

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "");
+    const int numRows = 3;
+    const int numCols = 5;
+
+    float arr[numRows][numCols];
+
+    for (int i = 0; i < numRows; i++) {
+        std::cout << "Введите элементы " << (i + 1) << "-й строки (через пробел): ";
+        for (int j = 0; j < numCols; j++) {
+            std::cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < numRows; i++) {
+        float sum = 0;
+        for (int j = 0; j < numCols; j++) {
+            sum += arr[i][j];
+        }
+        float average = sum / numCols;
+        std::cout << "Среднее арифметическое элементов " << (i + 1) << "-й строки: " << average << std::endl;
+    }
+
+    return 0;
+}
