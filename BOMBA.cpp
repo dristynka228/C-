6d0423264c63dd6fcd00fc93a99e8173ff6c3c1d
dristynka228 <144123819+dristynka228@users.noreply.github.com>
2024-05-31 12:26:38 +0300

    Add files via upload

---
+++ BOMBA.cpp
@@ -0,0 +1,13 @@
+﻿#include <cstring>
+#include <iostream>
+
+extern "C" __declspec(dllexport) bool CharSearch(const char* inputString, const char* searchChars) {
+    int len = strlen(searchChars);
+    for (int i = 0; i < len; i++) {
+        const char* found = strchr(inputString, searchChars[i]);
+        if (!found) {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ Test.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <locale>
+#include <Windows.h>
+using namespace std;
+
+typedef bool(*CharSearchFunction)(const char*, const char*);
+
+int main() {
+    setlocale(LC_ALL, "");
+    HINSTANCE hinstLib;
+    CharSearchFunction CharSearch;
+    
+    char inputString[100];
+    char inputChars[10];
+    cout << "Введите строку: ";
+    cin.getline(inputString, 100);
+    cout << "Введите символы для поиска: ";
+    cin.getline(inputChars, 10); 
+
+    hinstLib = LoadLibrary(TEXT("BOMBA.dll"));
+    if (hinstLib != NULL) {
+        
+        CharSearch = (CharSearchFunction)GetProcAddress(hinstLib, "CharSearch");
+        if (NULL != CharSearch) {
+          
+            bool result = CharSearch(inputString, inputChars);
+            if (result) {
+                cout << "буковка/слово найдено!!" << endl;
+            }
+            else {
+                cout << "буковка/слово не найдено:(" << endl;
+            }
+        }
+        
+        FreeLibrary(hinstLib);
+    }
+    return 0;
+}
